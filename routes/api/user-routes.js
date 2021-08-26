5f242444a811d44913ca986860382a3bf326fb29
Camil Grabowski <camil.grabowski@uconn.edu>
2021-08-26 16:43:20 -0400

    done with 13.1

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,19 +1,33 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// GET /api/users
+// // GET /api/users
+// router.get('/', (req, res) => {
+//     // Access our User model and run .findAll() method)
+//     User.findAll()
+//       .then(dbUserData => res.json(dbUserData))
+//       .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//       });
+//   });
+
+// GET /api/users to not return a password
 router.get('/', (req, res) => {
-    // Access our User model and run .findAll() method)
-    User.findAll()
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
+  .then(dbUserData => res.json(dbUserData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
   });
+});
+
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
+      attributes: {exclude: ['password']},
       where: {
         id: req.params.id
       }
