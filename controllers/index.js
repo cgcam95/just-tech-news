a3c790fce59a5ee650f772716d3a515650847edf
Camil Grabowski <camil.grabowski@uconn.edu>
2021-09-16 10:12:14 -0400

    homepage done

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const homeRoutes = require('./home-routes')
 
 const apiRoutes = require('./api'); // looking for hte index.js in the api folder
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 // turn on routes
 app.use(routes);
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
