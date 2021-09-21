02304be8e7eb10c166091439ce6618dc94db2a4d
Camil Grabowski <camil.grabowski@uconn.edu>
2021-09-21 13:44:12 -0400

    done

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -125,13 +125,16 @@ router.get('/:id', (req, res) => {
 // PUT /api/posts/upvote
 // Updated version of put /upvote using what was placed in Post.js
 router.put('/upvote', (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  // make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+      .then(updatedVoteData => res.json(updatedVoteData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
   
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,7 +30,10 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
       // pass a single post object into the homepage template
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('homepage', { posts });
+      res.render('homepage', { 
+        posts,
+      loggedIn: req.session.loggedIn 
+    });
     })
     .catch(err => {
       console.log(err);
@@ -79,7 +82,10 @@ router.get('/post/:id', (req, res) => {
       const post = dbPostData.get({ plain: true });
 
       // pass data to template
-      res.render('single-post', { post });
+      res.render('single-post', {
+        post,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch(err => {
       console.log(err);
